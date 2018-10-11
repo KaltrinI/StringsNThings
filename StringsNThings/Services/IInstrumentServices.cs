c499f113440e99c4291b58e63b4be54d8a0ef90d
KaltrinI <kaltrinismaili@gmail.com>
2018-10-11 20:57:24 +0200

    Implemented some methods of the InstrumentsService (untested)

---
+++ StringsNThings/Services/IInstrumentServices.cs
@@ -0,0 +1,16 @@
+﻿using StringsNThings.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace StringsNThings.Services
+{
+    public interface IInstrumentServices
+    {
+        List<Instrument> GetAllInstruments();
+        void AddInstrument(Instrument instrument);
+        Task<Instrument> GetInstrumentDetails(int id, string type);
+        Task<List<Instrument>> GetInstrumentsByType(string type);
+        void DeleteInstrument(Instrument instrument);
+        void ModifyInstrumentInfo(Instrument instrument);
+    }
+}
--- StringsNThings/Services/InstrumentServices.cs
+++ StringsNThings/Services/InstrumentServices.cs
@@ -1,13 +1,82 @@
 ﻿using StringsNThings.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
-using System.Web;
+using System.Threading.Tasks;
 
 namespace StringsNThings.Services
 {
-    public class InstrumentServices
+    public class InstrumentServices : IInstrumentServices
     {
         private InstrumentDBContext dBContext;
+
+        public void AddInstrument(Instrument instrument)
+        {
+            var table = GetInstrumentTable(instrument.Category);
+            table.Add(instrument);
+            dBContext.SaveChanges();
+        }
+
+        
+        public void DeleteInstrument(Instrument instrument)
+        {
+            var table = GetInstrumentTable(instrument.Category);
+            table.Remove(instrument);
+            dBContext.SaveChanges();
+        }
+
+        public List<Instrument> GetAllInstruments()
+        {
+            var instruments = dBContext.OtherInstruments.ToList();
+            instruments.AddRange(dBContext.BrassInstruments.ToList());
+            instruments.AddRange(dBContext.WoodwindInstruments.ToList());
+            instruments.AddRange(dBContext.KeyboardInstruments.ToList());
+            instruments.AddRange(dBContext.PercussionInstruments.ToList());
+            instruments.AddRange(dBContext.StringInstruments.ToList());
+            return instruments.OrderBy(x => x.Name).ToList();
+        }
+
+        public async Task<Instrument> GetInstrumentDetails(int id, string type)
+        {
+            var table = await GetInstrumentTable(type).ToListAsync();
+            var list = new List<Instrument>();
+            table.ForEach(x => list.Add((Instrument)x));
+            return list.First(a => a.Id == id);
+        }
+
+        public async Task<List<Instrument>> GetInstrumentsByType(string type)
+        {
+            
+            var table = await GetInstrumentTable(type).ToListAsync();
+            var list = new List<Instrument>();
+            table.ForEach(x => list.Add((Instrument)x));
+            return list;   
+        }
+
+        public void ModifyInstrumentInfo(Instrument instrument)
+        {
+            throw new NotImplementedException();
+        }
+
+        private DbSet GetInstrumentTable(string category)
+        {
+            switch (category)
+            {
+                case nameof(BrassInstrument):
+                    return dBContext.BrassInstruments;
+                case nameof(WoodwindInstrument):
+                    return dBContext.WoodwindInstruments;
+                case nameof(KeyboardInstrument):
+                    return dBContext.KeyboardInstruments;
+                case nameof(PercussionInstrument):
+                    return dBContext.PercussionInstruments;
+                case nameof(StringInstrument):
+                    return dBContext.StringInstruments;
+                default:
+                    return dBContext.OtherInstruments;
+            }
+        }
+
     }
 }
